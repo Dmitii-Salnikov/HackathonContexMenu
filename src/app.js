25bb7575f1a179c50eeb5bb4132df81cd825992f
AlekseyGurev <gurevaleksey@yandex.ru>
2023-12-02 21:30:56 +0300

    сепорировании методов в trigger

--- src/app.js
+++ src/app.js
@@ -1,9 +1,1 @@
 import './styles.css'
-import { ShapeModule } from './modules/shape.module'
-
-const shapeModule = new ShapeModule("ShapeModule", "Cоздать фигуру")
-shapeModule.trigger()
-
-document.addEventListener('click', () => {
-    shapeModule.trigger()
-})
