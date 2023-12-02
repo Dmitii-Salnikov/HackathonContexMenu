8eaff3e5a5196481af26a3b78f9d7906f594e905
Nikolay Gorokhov <nikolay.gorokhov@ya.ru>
2023-12-02 15:31:24 +0300

    edit style and timer

--- src/modules/timer.module.js
+++ src/modules/timer.module.js
@@ -1,7 +1,8 @@
 import {Module} from '../core/module'
 
 export class TimerModule extends Module {
-    trigger() {
+
+    #renderElement(){
         const container = document.createElement('div')
         container.className = 'container'
         document.body.append(container)
@@ -12,19 +13,31 @@ export class TimerModule extends Module {
         const timeValue = document.createElement('input')
         timeValue.id = 'number'
         timeValue.type = 'number'
-        timeValue.placeholder ='Ввеите число'
+        timeValue.min = 0
+        timeValue.placeholder ='Введите число'
+
         const timerBtn = document.createElement('input')
         timerBtn.type = 'submit'
         timerBtn.value = 'Старт'
 
         const timerValueBlock = document.createElement('div')
         timerValueBlock.className = 'timerValueBlock'
+
         const spanTimerInDiv = document.createElement('span')
+        spanTimerInDiv.textContent = 'Начинаем отсчет времени'
+
         timerValueBlock.append(spanTimerInDiv)
         
         timeForm.append(timeValue,timerBtn)
         timeValueBlock.append(timeForm)
         container.append(timeValueBlock)
+
+        return {timeForm, container, timerValueBlock, spanTimerInDiv}
+    }
+    
+    trigger() {        
+
+       const {timeForm, container, timerValueBlock, spanTimerInDiv} = this.#renderElement()
 
         timeForm.addEventListener('submit',(e)=>{
             e.preventDefault()
@@ -33,12 +46,13 @@ export class TimerModule extends Module {
             e.target.number.value = '' 
             
             const interval = setInterval(()=>{
+
                 if(timerValue === 0){
                     spanTimerInDiv.textContent = 'Отсчет времени завершен'
                     clearInterval(interval) 
                     setTimeout(()=>{
                         timerValueBlock.remove()
-                        spanTimerInDiv.textContent = ''
+                        spanTimerInDiv.textContent = 'Начинаем отсчет времени'
                     },1000)
                 } else{                                       
                     spanTimerInDiv.textContent = timerValue
